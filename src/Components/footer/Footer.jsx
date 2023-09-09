6589f7296edaa14b0ddb68af51fcdd00be01b1fb
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2023-09-09 16:51:44 -0300

    Inicio de sesion / Footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,10 +1,26 @@
+import {Link} from 'react-router-dom';
+
 
 export default function Footer() {
   return (
     <footer>
-      <div class="contenedor-final">
-        <p>&copy; Interiores Locos</p>
-      </div>
+      <div class="footer-content">
+            <div class="footer-logo">
+                <img src="logo.png" alt="Logo" width="100"/>
+            </div>
+            <div class="footer-links">
+                <ul>
+                    <li><a href="#">Aviso de copyright</a></li>
+                    <li><a href="#">Política de privacidad</a></li>
+                    <li><a href="#">Mapa de sitio</a></li>
+                </ul>
+            </div>
+            <div class="footer-contact">
+                <p>Información de contacto:</p>
+                <p>Email: info@example.com</p>
+                <p>Teléfono: +123456789</p>
+            </div>
+        </div>
     </footer>
   );
 }
--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -3,20 +3,46 @@ import './loginpage.css';
 export default function Loginpage() {
     return (
         <body>
-            <div className="container">
-                <h1><Link to='/login'>Iniciar Sesión</Link></h1>
-                <form>
-                    <div className="form-group">
-                        <label for="email">Email:</label>
-                        <input type="text" id="email" name="email" required/>
-                    </div>
-                    <div className="form-group">
-                        <label for="password">Contraseña:</label>
-                        <input type="password" id="password" name="password" required/>
-                    </div>
-                </form>
-                <div className="register-link">
-                    ¿No tienes una cuenta? <a href="">Regístrate</a>
+            <div class="container">
+                <div class="login-form">
+                    <h1>Iniciar Sesión</h1>
+                    <form>
+                        <div class="form-group">
+                            <label for="login-email">Email:</label>
+                            <input type="text" id="login-email" name="login-email" required/>
+                        </div>
+                        <div class="form-group">
+                            <label for="login-password">Contraseña:</label>
+                            <input type="password" id="login-password" name="login-password" required/>
+                        </div>
+                        <div class="form-group">
+                            <input type="button" value="Acceder"/>
+                        </div>
+                    </form>
+                </div>
+                <div class="register-form">
+                    <h1>Registrarse</h1>
+                    <form>
+                        <div class="form-group">
+                            <label for="register-email">Email:</label>
+                            <input type="text" id="register-email" name="register-email" required/>
+                        </div>
+                        <div class="form-group">
+                            <label for="register-password">Contraseña:</label>
+                            <input type="password" id="register-password" name="register-password" required/>
+                        </div>
+                        <div class="form-group">
+                            <label for="confirm-password">Repetir Contraseña:</label>
+                            <input type="password" id="confirm-password" name="confirm-password" required/>
+                        </div>
+                        <div class="form-group">
+                            <label for="direccion">Dirección de Vivienda:</label>
+                            <input type="text" id="direccion" name="direccion" required/>
+                        </div>
+                        <div class="form-group">
+                            <input type="button" value="Registrarse"/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </body>
