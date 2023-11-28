350ee9fbddbc4d2cbc15c8dbf04046b6cb793edf
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2023-11-28 01:20:20 -0300

    Conexion backend falta login y carrito

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -1,7 +1,6 @@
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
 import './loginpage.css';
-import axios from 'axios';
 
 
 export default function Loginpage() {
@@ -43,3 +42,68 @@ export default function Loginpage() {
         </body>
     )
 }
+
+// import { Link } from 'react-router-dom';
+// import { useState } from 'react';
+// import axios from 'axios';
+// import './loginpage.css';
+
+// export default function Loginpage() {
+//     const [passwordType, setPasswordType] = useState("");
+//     const [email, setEmail] = useState('');
+//     const [password, setPassword] = useState('');
+//     const [repeatPassword, setRepeatPassword] = useState('');
+
+//     const showPassword = (e) => {
+//         e.preventDefault();
+//         passwordType === "text" ? setPasswordType("password") : setPasswordType("text");
+//     };
+
+//     const loginpage = async (e) => {
+//         e.preventDefault();
+//         if (password === repeatPassword) {
+//             try {
+//                 await axios.post("http://localhost:8080/register", { email, password});
+//                 alert("Su cuenta ha sido creada exitosamente");
+//             } catch (error) {
+//                 alert("Hubo un error al crear la cuenta: " + error.message);
+//             }
+//         } else {
+//             alert("Sus contraseñas no coinciden");
+//         }
+//     }
+
+//     return (
+//         <body>
+//             <div className="container">
+//                 <h1>Iniciar Sesión</h1>
+//                 <form onSubmit={loginpage}>
+//                     <div className="form-group">
+//                         <label htmlFor="email">Email:</label>
+//                         <input type="text" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+//                     </div>
+//                     <div className="form-group">
+//                         <label htmlFor="password">Contraseña:</label>
+//                         <input type={passwordType} id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+//                     </div>
+
+//                     <div className="form-group">
+//                         <label htmlFor="confirm-password">Repetir Contraseña:</label>
+//                         <input type={passwordType} id="confirm-password" name="confirm-password" value={repeatPassword} onChange={(e) => setRepeatPassword(e.target.value)} required/>
+//                     </div>
+
+//                     <div className="form-group">
+//                         <button onClick={showPassword} type="submit">Mostrar Contraseña</button>
+//                     </div>
+
+//                     <div className="form-group">
+//                         <button type="submit">Registrarse</button>
+//                     </div>
+//                 </form>
+//                 <div className="register-link">
+//                     ¿No tienes una cuenta? <Link to='/register'>Regístrate</Link>
+//                 </div>
+//             </div>
+//         </body>
+//     )
+// }
