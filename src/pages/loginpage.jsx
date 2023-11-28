673ffd5d0a31056f69a63e5cc3475c6f39df0f0e
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2023-11-27 23:05:55 -0300

    falta conexion con login y carrito

--- src/pages/loginpage.jsx
+++ src/pages/loginpage.jsx
@@ -1,6 +1,7 @@
 import {Link} from 'react-router-dom';
 import {useState} from 'react';
 import './loginpage.css';
+import axios from 'axios';
 
 
 export default function Loginpage() {
--- src/pages/registerpage.jsx
+++ src/pages/registerpage.jsx
@@ -1,5 +1,31 @@
+import { useState } from 'react';
 import './registerpage.css';
+import axios from 'axios';
+
+
 export default function registerpage() {
+
+const [ name,nombre ]= useState("")
+const [ email,correoElectronico ] = useState("")
+const [ password,contraseña ] = useState("")
+const [ repeatpassword, repetircontraseña ] = useState("")
+const registro = async (e) => {
+
+    e.prevenDefault()
+    if (password === repeatPassword){
+        try {
+            await axios.post ("https://localhost:8080/usuarios",{email,password,name})
+            alert("Su cuenta ha sido creada exitosamente")
+        
+        } catch (error) {
+            alert("Su no ha podido ser creada")
+        }
+    }
+    else {
+        alert("Sus contraseñas no coinciden")
+    }
+}
+
     return (
     <body>
         <div class="container">
@@ -7,19 +33,19 @@ export default function registerpage() {
             <form>
                 <div class="form-group">
                     <label for="nombre">Nombre de Usuario:</label>
-                    <input type="text" id="nombre" name="nombre" required/>
+                    <input type="text" id="nombre" value = {name}  onChange={(e) => nombre (e.target.value)}  name="nombre" required/>
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
-                    <input type="text" id="email" name="email" required/>
+                    <input type="text" id="email" value = {email}  onChange={(e) => correoElectronico (e.target.value)} name="email" required/>
                 </div>
                 <div class="form-group">
                     <label for="password">Contraseña:</label>
-                    <input type="password" id="password" name="password" required/>
+                    <input type="password" id="password" value = {password}  onChange={(e) => password (e.target.value)} name="password" required/>
                 </div>
                 <div class="form-group">
                     <label for="confirm-password">Repetir Contraseña:</label>
-                    <input type="password" id="confirm-password" name="confirm-password" required/>
+                    <input type="password" id="confirm-password" value = {repeatpassword}  onChange={(e) => repetircontraseña (e.target.value)} name="confirm-password" required/>
                 </div>
                 <div class="form-group">
                     <button>Registrarse</button>
