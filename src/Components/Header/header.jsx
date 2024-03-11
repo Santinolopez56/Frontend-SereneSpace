77939dcfcb2bc62f3f595fee3b331d9200c1e443
Santinolopez56 <ss.lopez@alumno.etec.um.edu.ar>
2024-03-11 10:33:59 -0300

    Search Bar

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,5 +1,7 @@
 import plant from '../../multimedia/plant.png'
 import {Link} from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 import './header.css';
 
 export default function Header() {
@@ -16,6 +18,15 @@ export default function Header() {
             <div>
                 <nav>
                     <ul>
+                        <Autocomplete
+                            id="grouped-demo"
+                            options={options.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter))}
+                            groupBy={(option) => option.firstLetter}
+                            getOptionLabel={(option) => option.title}
+                            sx={{ width: 300 }}
+                            renderInput={(params) => <TextField {...params} label="With categories" />}
+                        />
+
                         <li><Link to='/'>Inicio</Link></li>
                         <li><Link to='/catalogo'>Catalogo</Link></li>
                         <li><Link to='/carrito'>Carrito</Link></li>
